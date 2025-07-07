5ba76d4f22ecd1208766cd8560a839a41cccc95b
Carmel Litmanovich <carmellit2002@gmail.com>
2025-07-07 15:04:28 +0300

    Created a hotel list component to display hotel cards

--- src/components/main-page-body/MainPageBody.tsx
+++ src/components/main-page-body/MainPageBody.tsx
@@ -1,9 +1,11 @@
 import { FunctionComponent } from "react";
 import { Container, Title } from "./styles";
+import HotelList from "../hotel-list";
 
 const MainPageBody: FunctionComponent = () => {
     return <Container>
         <Title> Select your ski trip </Title>
+        <HotelList />
     </Container>
 }
 
